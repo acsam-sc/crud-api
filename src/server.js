c71d1a805b91640c58287ea0360f63b7a1ffdb05
acsam-sc <acsam42@gmail.com>
2024-02-11 23:16:21 +0100

    feat: /GET and /POST done

---
+++ src/server.js
@@ -0,0 +1,63 @@
+import 'dotenv/config'
+import http from 'http'
+import { v4 as uuidv4, validate as uuidValidate } from 'uuid'
+
+const port = process.env.PORT || 3001
+let users = []
+const responseWithJSON = (res, json) => {
+	res.setHeader("Content-Type", "application/json")
+	res.write(JSON.stringify(json))
+	res.end()
+}
+const responseWithError = (res, errorCode, errorMessage) => {
+	res.statusCode = errorCode
+	res.setHeader("Content-Type", "text/html")
+	res.write(errorMessage)
+	res.end()
+}
+
+const server = http.createServer((req, res) => {
+if (req.url === '/api/users') {
+	if (req.method === "GET") {
+		res.statusCode = 200
+		responseWithJSON(res, users)
+		} else if (req.method === "POST") {
+			let reqBody = ''
+			req.on('data', chunk => {
+        reqBody += chunk.toString()
+    	})
+    	req.on('end', () => {
+				const { username, age, hobbies } = JSON.parse(reqBody)
+				if ( username && age && hobbies ) {
+					const newUser = { "id": uuidv4(), username, age, hobbies }
+					users.push(newUser)
+					res.statusCode = 201
+					responseWithJSON(res, users)
+				} else {
+					responseWithError(res, 400, "Request must contain username, age and hobbies")
+				}
+    })
+	}
+ } else if (req.url.startsWith('/api/users/') && req.method === "GET") {
+	const userId = req.url.slice(11)
+	let response
+	if (!uuidValidate(userId)) {
+		responseWithError(res, 400, "User ID is not correct uuid")
+	} else {
+		response = users.filter(it => it.id === userId)
+		if (response.length === 0) {
+			responseWithError(res, 404, "User doesn't exist")
+		} else {
+			res.statusCode = 200
+			responseWithJSON(res, response)
+		}
+	}
+ } else {
+		responseWithError(res, 404, "The requested resourse doesn't exist")
+	}
+
+})
+
+server.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+})
